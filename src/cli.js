df4b84d99dfe0cc8dc390b37dd1b864b683afa13
leafes <xmm.geek@gmail.com>
2022-07-01 20:32:32 +0700

    add and configure ESLint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const askUsername = ()  => {
-	const name = readlineSync.question("What's your name? ");
-	return name;
-}
+const askUsername = () => {
+  const name = readlineSync.question("What's your name? ");
+  return name; 
+};
+
+export default askUsername;

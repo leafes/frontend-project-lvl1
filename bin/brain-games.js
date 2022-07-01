2bc6c5eaaca6563c9cb6fbb4985ed48dc2643198
leafes <xmm.geek@gmail.com>
2022-07-01 15:17:24 +0700

    integrate askUsername function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import {askUsername} from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+
+const username = askUsername();
+
+console.log(`Hello, ${username}!`);

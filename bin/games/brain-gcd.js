a2926e637f126003ae2da3633ea273a0271eb467
leafes <xmm.geek@gmail.com>
2022-07-02 22:27:23 +0700

    eslint fixes

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -14,33 +14,31 @@ const MAX_NUMBER = 100;
 const questions = [];
 const correctAnswers = [];
 
-const divisors = (a) => { //поиск делителей числа
-	const result = [];
-	for (let i = a; i >= 1; i -= 1) {
-		if (a % i === 0) result.push(i);
-	}
-	return result;
-}
+const divisors = (a) => { // поиск делителей числа
+  const result = [];
+  for (let i = a; i >= 1; i -= 1) {
+    if (a % i === 0) result.push(i);
+  }
+  return result;
+};
 
 const greatestCommonDivisor = (aDivisors, bDivisors) => { // поиск большего общего делителя
-	const result = [];
-	for (const aDivisor of aDivisors) {
-		if (bDivisors.includes(aDivisor)) return aDivisor;
-	}
-	
-}
+  for (const aDivisor of aDivisors) {
+    if (bDivisors.includes(aDivisor)) return aDivisor;
+  }
+};
 
 for (let i = 0; i < 3; i += 1) {
-	const firstOperand = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER - 1) + MIN_NUMBER);
-	const secondOperand = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER - 1) + MIN_NUMBER);
-	const question = `${firstOperand}, ${secondOperand}`;
-	questions.push(question);
+  const firstOperand = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER - 1) + MIN_NUMBER);
+  const secondOperand = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER - 1) + MIN_NUMBER);
+  const question = `${firstOperand}, ${secondOperand}`;
+  questions.push(question);
 
-	const firstOperandDiv = divisors(firstOperand);
-	const secondOperandDiv = divisors(secondOperand);
-	let generatedAnswer = greatestCommonDivisor(firstOperandDiv, secondOperandDiv);
-	generatedAnswer = String(generatedAnswer);
-	correctAnswers.push(generatedAnswer);
+  const firstOperandDiv = divisors(firstOperand);
+  const secondOperandDiv = divisors(secondOperand);
+  let generatedAnswer = greatestCommonDivisor(firstOperandDiv, secondOperandDiv);
+  generatedAnswer = String(generatedAnswer);
+  correctAnswers.push(generatedAnswer);
 }
 
 generateGame(questions, correctAnswers, username);

ca532fb4807577301a4e29b4b692c1bcfdb342fc
leafes <xmm.geek@gmail.com>
2022-07-03 00:23:16 +0700

    eslint fixes

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -15,26 +15,26 @@ const questions = [];
 const correctAnswers = [];
 
 const generateProgression = () => {
-	const progression = [];
-	const step = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
-	const element = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
-	const length = generateRandomNumber(4, 11) * step;
+  const progression = [];
+  const step = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+  const element = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+  const length = generateRandomNumber(4, 11) * step;
 
-	for (let i = step; i <= length; i += step) {
-		progression.push(i + element);
-	}
+  for (let i = step; i <= length; i += step) {
+    progression.push(i + element);
+  }
 
-	return progression;
+  return progression;
 };
 
 for (let i = 0; i < 3; i += 1) {
-	let progression = generateProgression();
-	const hiddenIndex = Math.floor(Math.random() * progression.length)
-	const answer = String(progression[hiddenIndex]);
+  let progression = generateProgression();
+  const hiddenIndex = Math.floor(Math.random() * progression.length);
+  const answer = String(progression[hiddenIndex]);
 
-	progression[hiddenIndex] = '...';
-	progression = progression.join(' ');
-	questions.push(progression);
-	correctAnswers.push(answer);
+  progression[hiddenIndex] = '...';
+  progression = progression.join(' ');
+  questions.push(progression);
+  correctAnswers.push(answer);
 }
 generateGame(questions, correctAnswers, username);

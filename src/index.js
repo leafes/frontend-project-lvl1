9a5400efebdbf7e75edabd41b087fab0eb95841a
leafes <xmm.geek@gmail.com>
2022-07-03 17:21:34 +0700

    eslint fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import readlineSync from 'readline-sync';
 
 export const askUsername = () => {
-  const username = readlineSync.question("May I have your name? ");
+  const username = readlineSync.question('May I have your name? ');
   return username;
 };
-export const generateRandomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min - 1) + min);
-}
-export const generateGame = (questions, correctAnswers, username) => {
+export const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min - 1) + min);
 
+export const generateGame = (questions, correctAnswers, username) => {
   for (let i = 0; i < 3; i += 1) {
     console.log(`Question: ${questions[i]}`);
     const answer = readlineSync.question('Your answer: ');
@@ -17,12 +15,11 @@ export const generateGame = (questions, correctAnswers, username) => {
       console.log('Correct!');
 
       if (i === 2) return console.log(`Congratulations, ${username}!`);
-
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswers[i]}'.`);
 
       return console.log(`Let's try again, ${username}!`);
     }
   }
-  
+  return 0;
 };
